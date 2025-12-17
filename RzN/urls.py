765de053d287733179cd8067e680570826260ab8
Hamad Badar <hamadbadar5626@hotmail.com>
2025-12-17 18:31:21 +0500

    Revert "just testing, remember to revert"

--- RzN/urls.py
+++ RzN/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-player/', views.add_player, name='add_player'),
+    path('players/', views.players, name='players'),
+    path('players/<int:pk>/edit/', views.edit_player, name='edit_player'),
+    path('players/<int:pk>/delete/', views.delete_player, name='delete_player'),
     path('api/timezones/<int:country_id>/', views.country_timezones, name='country_timezones'),
 ]
--- RzN/views.py
+++ RzN/views.py
@@ -35,8 +35,9 @@ def home(request):
 
                 converted_times.append({
                     'name': p.name,
+                    'hq': p.hq_level,
                     'power': p.power,
-                    'country': p.country.name,
+                    'country': p.country,
                     'local_time': local_dt.strftime("%Y-%m-%d %H:%M"),
                     'time_class': time_class,
                 })
@@ -45,8 +46,6 @@ def home(request):
         "converted_times": converted_times,
         "selected_time": selected_time_str,
     })
-
-
 
 def add_player(request):
     countries = Country.objects.all().order_by('name')
@@ -84,3 +83,46 @@ def country_timezones(request, country_id):
     tzs = list(country.timezones.values('id', 'zone_name', 'gmt_offset', 'gmt_offset_name'))
     return JsonResponse(tzs, safe=False)
 
+def players(request):
+    sort = request.GET.get('sort')
+    qs = Player.objects.select_related('country', 'timezone')
+
+    if sort == 'power':
+        qs = qs.order_by('-power')
+    elif sort == 'hq':
+        qs = qs.order_by('-hq_level')
+    elif sort == 'name':
+        qs = qs.order_by('name')
+    elif sort == 'active':
+        qs = qs.order_by('-is_active')
+    elif sort == 'key':
+        qs = qs.order_by('-is_key_player')
+
+    return render(request, 'players.html', {'players': qs})
+
+
+def edit_player(request, pk):
+    player = Player.objects.get(pk=pk)
+    countries = Country.objects.all().order_by('name')
+
+    if request.method == 'POST':
+        player.name = request.POST['name']
+        player.hq_level = request.POST['hq_level']
+        player.power = request.POST['power']
+        player.country_id = request.POST['country']
+        player.timezone_id = request.POST.get('timezone')
+        player.is_active = 'is_active' in request.POST
+        player.is_key_player = 'is_key_player' in request.POST
+        player.save()
+
+        return redirect('players')
+
+    return render(request, 'edit_player.html', {
+        'player': player,
+        'countries': countries
+    })
+
+def delete_player(request, pk):
+    if request.method == 'POST':
+        Player.objects.filter(pk=pk).delete()
+    return redirect('players')
